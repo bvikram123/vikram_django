c98a47d62d518ef81b79d45bda47dbb0bbeb68f8
vikram <bodepudivikramkumr677@gmail.com>
2022-11-14 05:44:03 +0000

    done the change url

--- vikram_django/urls.py
+++ vikram_django/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-   path('star-wars/', include('my_api.urls')),
+   path('star-war/', include('my_api.urls')),
 ]
